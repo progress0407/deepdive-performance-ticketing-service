ddd8b293174f1604b728f6e8aa68a6059b887478
progress0407 <progress0407@gmail.com>
2023-06-13 11:25:51 +0900

    프로젝트 생성

---
+++ src/main/java/numble/deepdive/performanceticketingservice/PerformanceTicketingServiceApplication.java
@@ -0,0 +1,13 @@
+package numble.deepdive.performanceticketingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTicketingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceTicketingServiceApplication.class, args);
+	}
+
+}
