1e68de71bc2b5f917303656f82b208d4ee7015b4
progress0407 <progress0407@gmail.com>
2023-06-19 17:37:46 +0900

    공연장 리스트 조회 구현

--- src/main/java/numble/deepdive/performanceticketingservice/venue/presentation/VenueController.java
+++ src/main/java/numble/deepdive/performanceticketingservice/venue/presentation/VenueController.java
@@ -1,12 +1,20 @@
 package numble.deepdive.performanceticketingservice.venue.presentation;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import numble.deepdive.performanceticketingservice.venue.domain.Venue;
 import numble.deepdive.performanceticketingservice.venue.dto.VenueCreateRequest;
 import numble.deepdive.performanceticketingservice.venue.dto.VenueCreateResponse;
 import numble.deepdive.performanceticketingservice.venue.application.VenueService;
+import numble.deepdive.performanceticketingservice.venue.infrastructure.VenueRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RequestMapping("/venues")
 @RestController
@@ -14,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 public class VenueController {
 
     private final VenueService venueService;
+    private final VenueRepository venueRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -24,5 +33,41 @@ public class VenueController {
         long savedId = venueService.createVenue(entity);
 
         return new VenueCreateResponse(savedId);
+    }
+
+    // TODO GetMapping
+    @GetMapping
+    public VenueListResponses findAllVenues() {
+
+        List<Venue> venues = venueRepository.findAll();
+
+        List<VenueListResponse> venueCollections = venues.stream()
+                .map(VenueListResponse::new)
+                .collect(toList());
+
+        return new VenueListResponses(venueCollections);
+    }
+
+    @NoArgsConstructor
+    @Getter
+    public static class VenueListResponse {
+
+        private long id;
+        private String name;
+        private int seatCount;
+
+        public VenueListResponse(Venue venue) {
+            this.id = venue.getId();
+            this.name = venue.getName();
+            this.seatCount = venue.getSeats().size();
+        }
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class VenueListResponses {
+
+        private List<VenueListResponse> venues;
     }
 }
